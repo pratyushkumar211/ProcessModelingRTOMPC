87a4f693c109cef99ffb596b1bc5b0cebadb5a66
Pratyush Kumar <pratyushkumar@ucsb.edu>
2020-09-12 16:21:55 -0700

    add a script to make plots.

---
+++ threereac_plots.py
@@ -0,0 +1,16 @@
+""" Script to plot the grey-box + NN model predictions.
+    Pratyush Kumar, pratyushkumar@ucsb.edu """
+
+import sys
+sys.path.append('lib/')
+import matplotlib.pyplot as plt
+import 
+
+
+
+
+def _plot_model_predictions():
+
+
+
+    return 
