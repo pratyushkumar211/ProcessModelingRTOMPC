a3641778b900cbc0446c9663d5f2ca3829ca9910
Pratyush Kumar <pratyushkumar@ucsb.edu>
2020-09-12 18:59:29 -0700

    add a script for training.

---
+++ threereac_train.py
@@ -0,0 +1,8 @@
+""" Script to train the hybrid model for the 
+    three reaction system. 
+    Pratyush Kumar, pratyushkumar@ucsb.edu """
+
+import sys
+sys.path.append('lib/')
+import numpy as np
+from hybridid import PickleTool
