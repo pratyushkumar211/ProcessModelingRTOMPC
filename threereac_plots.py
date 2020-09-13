4b1b546f331b920237542b417967f8a93222b398
Pratyush Kumar <pratyushkumar@ucsb.edu>
2020-09-12 18:08:43 -0700

    add the plotting script.

--- threereac_plots.py
+++ threereac_plots.py
@@ -12,7 +12,8 @@ from hybridid import (PickleTool, PRESENTATION_FIGSIZE)
 def plot_training_data(*, training_data, plot_range,
                           figure_size=PRESENTATION_FIGSIZE,
                           ylabel_xcoordinate=-0.1, 
-                          linewidth=0.8):
+                          linewidth=0.8, 
+                          markersize=1.):
     """ Plot the performance loss economic MPC parameters."""
     (figure, axes_array) = plt.subplots(nrows=5, ncols=1, 
                                         sharex=True, 
@@ -27,10 +28,11 @@ def plot_training_data(*, training_data, plot_range,
     data_list = [training_data.Ca, training_data.Cb, 
                  training_data.Cc, training_data.Cd, training_data.Ca0]
     for (axes, data, ylabel) in zip(axes_array, data_list, ylabels):
-        if ylabel == '$C_c(\textnormal{mol/m}^3)':
-            axes.plot(time, data[start:end], 'o')
+        if ylabel == r'$C_c(\textnormal{mol/m}^3)':
+            axes.plot(time[start:end], data[start:end], 'o', 
+                      markersize=markersize)
         else:
-            axes.plot(time, data[start:end])
+            axes.plot(time[start:end], data[start:end])
         axes.set_ylabel(ylabel)
         axes.get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5)
     axes.set_xlabel('Time (hr)')
@@ -42,7 +44,8 @@ def plot_val_model_predictions(*, plantsim_data,
                                   modelsim_datum, plot_range,
                                   figure_size=PRESENTATION_FIGSIZE,
                                   ylabel_xcoordinate=-0.1, 
-                                  linewidth=0.8):
+                                  linewidth=0.8, 
+                                  markersize=1.):
     """ Plot the performance loss economic MPC parameters."""
     (figure, axes_array) = plt.subplots(nrows=4, ncols=1, 
                                         sharex=True, 
@@ -52,37 +55,44 @@ def plot_val_model_predictions(*, plantsim_data,
                r'$C_c \ (\textnormal{mol/m}^3)$',
                r'$C_d \ (\textnormal{mol/m}^3)$',
                r'$C_{a0} \ (\textnormal{mol/m}^3)$']
-    legend_colors = ['b', 'g']
+    legend_colors = ['g']
     legend_handles = []
+    plant_data_list = [plantsim_data.Ca, plantsim_data.Cc, 
+                       plantsim_data.Cd, plantsim_data.Ca0]
     for (modelsim_data, 
          legend_color) in zip(modelsim_datum, legend_colors):
-        time = cascadesim_data.time/3600
-        data_list = [(cascadesim_data.Tzone, cascadesim_data.Tzone_sp), 
-                      cascadesim_data.upi, cascadesim_data.i]
-        for (axes, data, ylabel) in zip(axes_array, data_list, ylabels):
-            if ylabel == '$T_{z}(^\circ$C)':
-                (Tzone, Tsp) = data
-                legend_temp_handle = axes.plot(time, Tzone, legend_color)
-                axes.plot(time, Tsp, '--' + legend_color)
-                axes.plot(time, cascadesim_data.Tzone_lb, 'k')
-                axes.plot(time, cascadesim_data.Tzone_ub, 'k')
-            elif ylabel == '$\dot{Q}_c$ (kW)':
-                axes.plot(time, data, legend_color)
-                axes.plot(time, cascadesim_data.Qcool_lb, 'k')
-                axes.plot(time, cascadesim_data.Qcool_ub, 'k')
+        time = modelsim_data.time/60
+        model_data_list = [modelsim_data.Ca, modelsim_data.Cc, 
+                           modelsim_data.Cd, modelsim_data.Ca0]
+        for (axes, plantdata, 
+             modeldata, ylabel) in zip(axes_array, plant_data_list, 
+                                       model_data_list, ylabels):
+            if ylabel == r'$C_c \ (\textnormal{mol/m}^3)$':
+                axes.plot(time[start:end], plantdata[start:end], 'bo', 
+                           markersize=markersize)
+                legend_temp_handle = axes.plot(time[start:end], 
+                                        modeldata[start:end], legend_color)
             else:
-                axes.plot(time, data, legend_color)
-            axes.set_ylabel(ylabel, rotation=False)
+                legend_temp_handle = axes.plot(time[start:end], 
+                                               plantdata[start:end], 'b')
+                axes.plot(time[start:end], 
+                          modeldata[start:end], legend_color)
+            axes.set_ylabel(ylabel)
             axes.get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5)
-        legend_handles += legend_temp_handle 
+            legend_handles += legend_temp_handle 
     axes.set_xlabel('Time (hr)')
     axes.set_xlim([np.min(time), np.max(time)])
     figure.legend(handles = legend_handles,
-                  labels = (r'$\alpha$ = 30',
-                            r'$\alpha$ = 50'), 
-                  loc = (0.32, 0.9), ncol=3)
+                  labels = ('Plant', 'Grey-box'), 
+                  loc = (0.32, 0.9), ncol=2)
     # Return the figure object.
     return [figure]
+
+def plot_idmodel_vs_training_data():
+    """ Function to plot the data requirements 
+        of the hybrid model. """
+    None
+    return 
 
 def main():
     """ Load the pickle file and plot. """
@@ -93,6 +103,10 @@ def main():
     figures += plot_training_data(training_data=
                                   threereac_parameters['train_val_datum'][0], 
                                   plot_range=(0, 24*60))
+    figures += plot_val_model_predictions(plantsim_data=
+                                  threereac_parameters['train_val_datum'][1],
+                    modelsim_datum=[threereac_parameters['greybox_val_data']],
+                    plot_range=(0, 8*60))
     with PdfPages('threereac_plots.pdf', 
                   'w') as pdf_file:
         for fig in figures:
