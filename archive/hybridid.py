63f803bbf5bf07e8666a0818994b16937b4cf700
Pratyush Kumar <pratyushkumar@ucsb.edu>
2020-09-13 21:04:58 -0700

    ok, cleanup.

---
+++ archive/hybridid.py
@@ -0,0 +1,110 @@
+# Pratyush Kumar, pratyushkumar@ucsb.edu
+
+import sys
+import numpy as np
+import mpctools as mpc
+import pandas as pd
+import scipy.linalg
+import cvxopt as cvx
+import matplotlib.pyplot as plt
+import casadi
+import collections
+import pickle
+import plottools
+
+FIGURE_SIZE_A4 = (9, 10)
+PRESENTATION_FIGSIZE = (6, 6)
+PAPER_FIGSIZE = (7, 5)
+
+PlantSimData = collections.namedtuple('PlantSimData', 
+                    ['time', 'Ca', 'Cb', 'Cc', 
+                     'Cd', 'Ca0'])
+
+ModelSimData = collections.namedtuple('ModelSimData', 
+                    ['time', 'Ca', 'Cc', 'Cd', 'Ca0'])
+
+class PickleTool:
+    """Class which contains a few static methods for saving and
+    loading pkl data files conveniently."""
+    @staticmethod
+    def load(filename, type='write'):
+        """Wrapper to load data."""
+        if type == 'read':
+            with open(filename, "rb") as stream:
+                return pickle.load(stream)
+        if type == 'write':
+            with open(filename, "wb") as stream:
+                return pickle.load(stream)
+    
+    @staticmethod
+    def save(data_object, filename):
+        """Wrapper to pickle a data object."""
+        with open(filename, "wb") as stream:
+            pickle.dump(data_object, stream)
+
+class NonlinearPlantSimulator:
+    """Custom class for simulating non-linear plants."""
+    def __init__(self, *, fxup, hx, Rv, Nx, Nu, Np, Ny, 
+                 sample_time, x0):
+        
+        # Set attributes.
+        self.fxup = mpc.DiscreteSimulator(fxup, sample_time,
+                                          [Nx, Nu, Np], ["x", "u", "p"])
+        self.hx = mpc.getCasadiFunc(hx, [Nx], ["x"], funcname="hx")
+        (self.Nx, self.Nu, self.Ny, self.Np) = (Nx, Nu, Ny, Np)
+        self.measurement_noise_std = np.sqrt(np.diag(Rv)[:, np.newaxis])
+        self.sample_time = sample_time
+
+        # Create lists to save data.
+        self.x = [x0]
+        self.u = []
+        self.p = []
+        self.y = [np.asarray(self.hx(x0)) + 
+                 self.measurement_noise_std*np.random.randn(self.Ny, 1)]
+        self.t = [0.]
+
+    def step(self, u, p):
+        """ Inject the control input into the plant."""
+        x = self.fxup.sim(self.x[-1], u, p)[:, np.newaxis]
+        y = np.asarray(self.hx(x))
+        y = y + self.measurement_noise_std*np.random.randn(self.Ny, 1)
+        self._append_data(x, u, p, y)
+        return y
+
+    def _append_data(self, x, u, p, y):
+        """ Append the data into the lists.
+        Used for plotting in the specific subclasses.
+        """
+        self.x.append(x)
+        self.u.append(u)
+        self.p.append(p)
+        self.y.append(y)
+        self.t.append(self.t[-1]+self.sample_time)
+
+def _sample_repeats(num_change, num_simulation_steps,
+                       mean_change, sigma_change):
+    """ Sample the number of times a repeat in each
+    of the sampled vector is required."""
+    repeat = sigma_change*np.random.randn(num_change-1) + mean_change
+    repeat = np.floor(repeat)
+    repeat = np.where(repeat<=0., 0., repeat)
+    repeat = np.append(repeat, num_simulation_steps-np.int(np.sum(repeat)))
+    return repeat.astype(int)
+
+def sample_prbs_like(*, num_change, num_steps, 
+                    lb, ub, mean_change, sigma_change, seed=1):
+    """Sample a PRBS like sequence.
+    num_change: Number of changes in the signal.
+    num_simulation_steps: Number of steps in the signal.
+    mean_change: mean_value after which a 
+                 change in the signal is desired.
+    sigma_change: standard deviation of changes in the signal.
+    """
+    signal_dimension = lb.shape[0]
+    lb = lb.squeeze() # Squeeze the vectors.
+    ub = ub.squeeze() # Squeeze the vectors.
+    np.random.seed(seed)
+    values = (ub-lb)*np.random.rand(num_change, signal_dimension) + lb
+    repeat = _sample_repeats(num_change, num_steps,
+                             mean_change, sigma_change)
+    return np.repeat(values, repeat, axis=0)
