c985978099275259eb14cb871a680c61d6f67a0d
Pratyush Kumar <pratyushkumar@ucsb.edu>
2020-09-25 12:01:44 -0700

    add python scripts.

--- lib/HybridModelLayers.py
+++ lib/HybridModelLayers.py
@@ -17,18 +17,23 @@ class TwoReacCell(tf.keras.layers.AbstractRNNCell):
     RNN Cell
     dxG/dt  = fG(xG, u) + f_{NN}(y_{k+1-N_p:k}, u_{k+1-N_p:k-1}), y = xG
     """
-    def __init__(self, fnn_layers, tworeac_parameters, **kwargs):
-        super(TwoReacHybridCell, self).__init__(**kwargs)
+    def __init__(self, Np, interp_layer, 
+                       fnn_layers, tworeac_parameters, **kwargs):
+        super(TwoReacCell, self).__init__(**kwargs)
+        self.Np = Np
+        self.interp_layer = interp_layer
         self.fnn_layers = fnn_layers
         self.tworeac_parameters = tworeac_parameters
+        (self.Ng, self.Nu) = (tworeac_parameters['Ng'],
+                              tworeac_parameters['Nu'])
 
     @property
     def state_size(self):
-        return self.tworeac_parameters['Ng']
+        return self.Ng
     
     @property
     def output_size(self):
-        return self.tworeac_parameters['Ng']        
+        return self.Ng        
 
     def _fg(self, x, u):
         """ Function to compute the 
@@ -37,35 +42,22 @@ class TwoReacCell(tf.keras.layers.AbstractRNNCell):
 
         # Extract the parameters.
         k1 = self.tworeac_parameters['k1']
-        tau = self.tworeac_parameters['tau']
-
-        # The concentrations.
-        (Ca, Cd, Cc) = (x[..., 0:1], x[..., 1:2], x[..., 2:3])
+        tau = self.tworeac_parameters['ps'].squeeze()
+        
+        # Get the state and control.
+        (Ca, Cb) = (x[..., 0:1], x[..., 1:2])
         Ca0 = u[..., 0:1]
         
-        # Define a constant.
-        one = tf.constant(1., shape=(), dtype='float64')
-        sqrtoneplusbetaCa = tf.math.sqrt(tf.math.add(one, beta*Ca))
-
         # Write the ODEs.
-        dCabydt = F*tf.math.subtract(Ca0, Ca)/Vr
-        dCabydt = tf.math.subtract(dCabydt, k1*Ca)
-
-        dCdbydt1 = 0.5*k1*Ca
-        dCdbydt2 = tf.math.divide(tf.math.subtract(sqrtoneplusbetaCa, one), 
-                                  tf.math.add(sqrtoneplusbetaCa, one))
-        dCdbydt = tf.math.multiply(dCdbydt1, dCdbydt2) 
-        dCdbydt = tf.math.subtract(dCdbydt, F*Cd/Vr)
-
-        dCcbydt = 2*k1*tf.math.divide(Ca,  tf.math.add(one, sqrtoneplusbetaCa)) 
-        dCcbydt = tf.math.subtract(dCcbydt, F*Cc/Vr)
+        dCabydt = (Ca0 - Ca)/tau - k1*Ca
+        dCbbydt = k1*Ca - Cb/tau
 
         # Return the derivative.
-        return tf.concat((dCabydt, dCdbydt, dCcbydt), axis=-1)
+        return tf.concat([dCabydt, dCbbydt], axis=-1)
 
-    def _fnn(self, yseq, useq):
+    def _fnn(self, ypseq, upseq):
         """ Compute the output of the feedforward network. """
-        fnn_output = tf.concat((yseq, useq), axis=-1)
+        fnn_output = tf.concat((ypseq, upseq), axis=-1)
         for layer in self.fnn_layers:
             fnn_output = layer(fnn_output)
         return fnn_output
@@ -73,28 +65,27 @@ class TwoReacCell(tf.keras.layers.AbstractRNNCell):
     def call(self, inputs, states):
         """ Call function of the hybrid RNN cell.
             Dimension of xG: (None, Ng)
-            Dimension of u: (None, Nu)
-            Dimension of yseq: (None, (Np-1)*p)
-            Dimension of useq: (None, (Np-1)*m)
+            Dimension of input: (None, Nu+(Np-1)*(Ng+Nu))
         """
         # Extract important variables.
         [xG] = states
-        [u, yseq, useq] = inputs
-        Delta = self.threereac_parameters['sample_time']
+        [u, ypseq, upseq] = tf.split(inputs, [self.Nu, 
+                            (self.Np-1)*self.Ng, (self.Np-1)*self.Nu], axis=-1)
+        Delta = self.tworeac_parameters['sample_time']
 
         # Write-down the RK4 step for the NN grey-box augmentation.
-        yseq_fnn = tf.concat((xG, yseq), axis=-1)
-        k1 = self._fg(xG, u) + self._fnn(yseq_fnn, useq)
-
-        yseq_interp = self.interp_layer(yseq_fnn)
-        yseq_fnn = tf.concat((xG + Delta*(k1/2), yseq_interp), axis=-1)
-        k2 = self._fg(xG + Delta*(k1/2), u) + self._fnn(yseq_fnn, useq)
+        ypseq_fnn = tf.concat((xG, ypseq), axis=-1)
+        k1 = self._fg(xG, u) + self._fnn(ypseq_fnn, upseq)
         
-        yseq_fnn = tf.concat((xG + Delta*(k2/2), yseq_interp), axis=-1)
-        k3 = self._fg(xG + Delta*(k2/2), u) + self._fnn(yseq_fnn, useq)
+        ypseq_interp = self.interp_layer(ypseq_fnn)
+        ypseq_fnn = tf.concat((xG + Delta*(k1/2), ypseq_interp), axis=-1)
+        k2 = self._fg(xG + Delta*(k1/2), u) + self._fnn(ypseq_fnn, upseq)
         
-        yseq_fnn = tf.concat((xG + Delta*k3, yseq_interp), axis=-1)
-        k4 = self._fg(xG + Delta*k3, u) + self._fnn(yseq_fnn, useq)
+        ypseq_fnn = tf.concat((xG + Delta*(k2/2), ypseq_interp), axis=-1)
+        k3 = self._fg(xG + Delta*(k2/2), u) + self._fnn(ypseq_fnn, upseq)
+        
+        ypseq_fnn = tf.concat((xG + Delta*k3, ypseq_interp), axis=-1)
+        k4 = self._fg(xG + Delta*k3, u) + self._fnn(ypseq_fnn, upseq)
         
         # Get the current output/state and the next time step.
         y = xG
@@ -119,8 +110,8 @@ class InterpolationLayer(tf.keras.layers.Layer):
         """
         yseq_interp = []
         for t in range(self.Np-1):
-            yseq_interp.append(0.5*(yseq[..., t*n:(t+1)*n] + 
-                                    yseq[..., (t+1)*n:(t+2)*n]))
+            yseq_interp.append(0.5*(yseq[..., t*self.p:(t+1)*self.p] + 
+                                    yseq[..., (t+1)*self.p:(t+2)*self.p]))
         return tf.concat(yseq_interp, axis=-1)
 
     def get_config(self):
@@ -128,15 +119,18 @@ class InterpolationLayer(tf.keras.layers.Layer):
 
 class TwoReacModel(tf.keras.Model):
     """ Custom model for the Two reaction model. """
-    def __init__(self, fnn_dims, tworeac_parameters):
+    def __init__(self, Np, fnn_dims, tworeac_parameters):
         """ Create the dense layers for the NN, and 
             construct the overall model. """
-        (Ng, Nu) = (tworeac_parameters['Ng'], tworeac_parameters['Nu'])
 
-        # Input layers.
-        inputs = [tf.keras.Input(name='u', shape=(None, Nu)),
-                  tf.keras.Input(name='yseq', shape=(None, Ng)),
-                  tf.keras.Input(name='useq', shape=(None, Nu))]
+        # Get the size and input layer, and initial state layer.
+        (Ng, Nu) = (tworeac_parameters['Ng'], tworeac_parameters['Nu'])
+        layer_input = tf.keras.Input(name='u_ypseq_upseq', 
+                        shape=(None, Nu+(Np-1)*(Ng+Nu)))
+        initial_state = tf.keras.Input(name='x0', shape=(Ng, ))
+
+        # Get the interpolation layer.
+        interp_layer = InterpolationLayer(p=Ng, Np=Np)
 
         # Dense layers for the black-box NN.
         fnn_layers = []
@@ -145,7 +139,10 @@ class TwoReacModel(tf.keras.Model):
         fnn_layers.append(tf.keras.layers.Dense(fnn_dims[-1]))
 
         # Construct the RNN layer and the model.
-        tworeac_cell = TwoReacCell(fnn_layers, tworeac_parameters)
+        tworeac_cell = TwoReacCell(Np, interp_layer, 
+                                   fnn_layers, tworeac_parameters)
         tworeac_layer = tf.keras.layers.RNN(tworeac_cell, return_sequences=True)
-        outputs = tworeac_layer(inputs)
-        super().__init__(inputs=inputs, outputs=outputs)
+        layer_output = tworeac_layer(inputs=layer_input, 
+                                initial_state=[initial_state])
+        super().__init__(inputs=[layer_input, initial_state], 
+                         outputs=layer_output)
--- tworeac_train.py
+++ tworeac_train.py
@@ -7,37 +7,35 @@ sys.path.append('lib/')
 import time
 import tensorflow as tf
 import numpy as np
-from hybridid import (PickleTool, ModelSimData)
-from HybridModelLayers import get_threereac_model
-from threereac_parameters import _get_threereac_parameters
+from hybridid import PickleTool
+from HybridModelLayers import TwoReacModel
 
 # Set the tensorflow graph-level seed.
 tf.random.set_seed(1)
 
-def create_hybrid_model(*, threereac_parameters):
-    """ Create/compile the hybrid model for training."""
-    bb_dims = [6, 64, 64, 2]
-    hybrid_model = get_threereac_model(threereac_parameters=
-                                       threereac_parameters, 
-                                       bb_dims=bb_dims)
+def create_tworeac_model(*, Np, fnn_dims, tworeac_parameters):
+    """ Create/compile the two reaction model for training."""
+    tworeac_model = TwoReacModel(Np=Np,
+                                 fnn_dims=fnn_dims,
+                                 tworeac_parameters=tworeac_parameters)
     # Compile the nn controller.
-    hybrid_model.compile(optimizer=
-                         tf.keras.optimizers.Adam(learning_rate=1e-3), 
+    tworeac_model.compile(optimizer='adam', 
                          loss='mean_squared_error')
+    breakpoint()
     # Return the compiled model.
-    return hybrid_model
+    return tworeac_model
 
-def get_data_for_training(*, train_val_datum, num_batches=32):
-    """ Get the data for training. """
-    inputs, outputs = [], []
-    for i in range(num_batches):
-        inputs += [train_val_datum[i].Ca0[np.newaxis, :, np.newaxis]]
-        outputs += [train_val_datum[i].Cc[np.newaxis, :, np.newaxis]]
-    train_data = dict(u=np.concatenate(inputs, axis=0),
-                      y=np.concatenate(outputs, axis=0))
-    trainval_data = dict(u=train_val_datum[32].Ca0[np.newaxis, :, np.newaxis],
-                         y=train_val_datum[32].Cc[np.newaxis, :, np.newaxis])
-    return (train_data, trainval_data)
+#def get_data_for_training(*, train_val_datum, num_batches=32):
+#    """ Get the data for training. """
+#    inputs, outputs = [], []
+#    for i in range(num_batches):
+#        inputs += [train_val_datum[i].Ca0[np.newaxis, :, np.newaxis]]
+#        outputs += [train_val_datum[i].Cc[np.newaxis, :, np.newaxis]]
+#    train_data = dict(u=np.concatenate(inputs, axis=0),
+#                      y=np.concatenate(outputs, axis=0))
+#    trainval_data = dict(u=train_val_datum[32].Ca0[np.newaxis, :, np.newaxis],
+#                         y=train_val_datum[32].Cc[np.newaxis, :, np.newaxis])
+#    return (train_data, trainval_data)
 
 def train_nn_controller(hybrid_model, train_data, trainval_data, 
                         stdout_filename, ckpt_path):
@@ -63,25 +61,27 @@ def train_nn_controller(hybrid_model, train_data, trainval_data,
     # Return the NN controller.
     return (hybrid_model, training_time)
 
-def get_prediction_data(hybrid_model, val_data, 
-                        train_val_datum):
-    """ Predict for plotting. """
-    pred = hybrid_model.predict(x=[val_data['u']]).squeeze()
-    return ModelSimData(time=train_val_datum[2].time, 
-                Ca=train_val_datum[2].Ca, Cc=pred, 
-                Cd=train_val_datum[2].Cd,
-                Ca0=train_val_datum[2].Ca0)
+#def get_prediction_data(hybrid_model, val_data, 
+#                        train_val_datum):
+#    """ Predict for plotting. """
+#    pred = hybrid_model.predict(x=[val_data['u']]).squeeze()
+#    return ModelSimData(time=train_val_datum[2].time, 
+#                Ca=train_val_datum[2].Ca, Cc=pred, 
+#                Cd=train_val_datum[2].Cd,
+#                Ca0=train_val_datum[2].Ca0)
 
 def main():
     """ Main function to be executed. """
 
     # Load data.
-    threereac_parameters = PickleTool.load(filename=
-                                    'threereac_parameters.pickle',
-                                    type='read')
+    tworeac_parameters = PickleTool.load(filename='tworeac_parameters.pickle',
+                                         type='read')
     # Create the hybrid model.
-    hybrid_model = create_hybrid_model(threereac_parameters=
-                                       threereac_parameters['parameters'])
+    Np = 5
+    fnn_dims = [14, 64, 64, 2]
+    hybrid_model = create_tworeac_model(Np=Np, fnn_dims=fnn_dims,
+                    tworeac_parameters=tworeac_parameters['parameters'])
+    breakpoint()
     # Get the training data.
     (train_data, 
         trainval_data) = get_data_for_training(train_val_datum=
