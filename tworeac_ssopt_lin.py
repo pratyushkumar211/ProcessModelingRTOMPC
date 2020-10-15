516bc587bcb6e0bbab9df29b74d379768f94091b
Pratyush Kumar <pratyushkumar@ucsb.edu>
2020-10-15 12:59:36 -0700

    ok, let's do this for the nonlinear model.

--- tworeac_ssopt_lin.py
+++ tworeac_ssopt_lin.py
@@ -1,62 +1,95 @@
 # [depends] %LIB%/hybridid.py tworeac_parameters_lin.pickle
 # [makes] pickle
-""" Script to train the hybrid model for the 
-    three reaction system. 
+""" Script to use the trained hybrid model for 
+    steady-state optimization.
     Pratyush Kumar, pratyushkumar@ucsb.edu """
 
 import sys
 sys.path.append('lib/')
-import tensorflow as tf
-import time
 import numpy as np
-from hybridid import (PickleTool, get_tworeac_train_val_data, 
-                      SimData)
-from HybridModelLayers import TwoReacModel
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from hybridid import (PickleTool, PAPER_FIGSIZE)
 
-# Set the tensorflow graph-level seed.
-tf.random.set_seed(1)
+def _plant_ys(*, us, parameters):
+    """ Return the plant xs. 
+        Solve: Ax + Bu = 0 (continuous time SS) 
+    """
+    # Get the parameters.
+    k1 = parameters['k1']
+    k2 = parameters['k2']
+    k3 = parameters['k3']
+    Ny = parameters['Ny']
+    tau = parameters['ps'].squeeze()
+    # Get the matrices and xs. 
+    A = np.array([[-k1-(1/tau), 0., 0.], 
+                  [k1, -k2 - (1/tau), k3], 
+                  [0., k2, -k3 -(1/tau)]])
+    B = np.array([[1/tau], [0.], [0.]]) 
+    xs = -np.linalg.inv(A) @ (B @ us)
+    # Return.
+    return xs[0:Ny]
 
-def create_tworeac_model(*, Np, fnn_dims, tworeac_parameters):
-    """ Create/compile the two reaction model for training. """
-    tworeac_model = TwoReacModel(Np=Np,
-                                 fnn_dims=fnn_dims,
-                                 tworeac_parameters=tworeac_parameters)
-    # Compile the nn controller.
-    tworeac_model.compile(optimizer='adam', 
-                          loss='mean_squared_error')
+def _greybox_ys(*, us, parameters):
+    """ Return the plant xs. 
+        Solve: Ax + Bu = 0 (continuous time SS) 
+    """
+    # Get the parameters.
+    k1 = parameters['k1']
+    Ny = parameters['Ny']
+    tau = parameters['ps'].squeeze()
+    # Get the matrices and xs. 
+    A = np.array([[-k1-(1/tau), 0.], 
+                  [k1, -(1/tau)]])
+    B = np.array([[1/tau], [0.]]) 
+    xs = -np.linalg.inv(A) @ (B @ us)
+    # Return.
+    return xs[0:Ny]
+
+def cost(ys, us):
+    """ Compute the steady-state cost. """
+    (cost, profit) = (100, 150)
+    # Return.
+    return cost*us - profit*ys[:, -1:]
+
+def plot_profit_curve(*, us, costs, figure_size=PAPER_FIGSIZE, 
+                         ylabel_xcoordinate=-0.12, 
+                         left_label_frac=0.15):
+    """ Plot the profit curves. """
+    (figure, axes) = plt.subplots(nrows=1, ncols=1, 
+                                        sharex=True, 
+                                        figsize=figure_size, 
+                                    gridspec_kw=dict(left=left_label_frac))
+    xlabel = r'$C_{A0} \ (\textnormal{mol/m}^3)$'
+    ylabel = r'Cost ($\$ $)'
+    colors = ['b', 'g']
+    legends = ['Plant', 'Grey-box']
+    for (cost, color) in zip(costs, colors):
+        # Plot the corresponding data.
+        axes.plot(us, cost, color)
+    axes.legend(legends)
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel, rotation=False)
+    axes.get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5) 
+    axes.set_xlim([np.min(us), np.max(us)])
+    # Return the figure object.
+    return [figure]
+
+def compute_cost_curves(*, parameters):
+    """ Compute the profit curves for the three models. """
+    (ulb, uub) = (parameters['lb']['u'], parameters['ub']['u'])
+    uss = np.linspace(ulb, uub, 100)[:, np.newaxis]
+    (ys_plant, ys_greybox) = ([], [])
+    for us in uss:
+        ys_plant.append(_plant_ys(us=us, parameters=parameters))
+        ys_greybox.append(_greybox_ys(us=us, 
+                                      parameters=parameters))
+    ys_plant = np.asarray(ys_plant).squeeze()
+    ys_greybox = np.asarray(ys_greybox).squeeze()
+    us = uss.squeeze(axis=-1)
+    (cost_plant, cost_greybox) = (cost(ys_plant, us), cost(ys_greybox, us))
     # Return the compiled model.
-    return tworeac_model
-
-def train_model(model, train_data, trainval_data, val_data,
-                stdout_filename, ckpt_path):
-    """ Function to train the NN controller."""
-    # Std out.
-    sys.stdout = open(stdout_filename, 'w')
-    # Create the checkpoint callback.
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path,
-                                                    monitor='val_loss',
-                                                    save_best_only=True,
-                                                    save_weights_only=True,
-                                                    verbose=1)
-    # Call the fit method to train.
-    tstart = time.time()
-    model.fit(x=[train_data['inputs'], train_data['x0']], 
-              y=train_data['outputs'], 
-            epochs=1000, batch_size=1,
-            validation_data = ([trainval_data['inputs'], trainval_data['x0']], 
-                                trainval_data['outputs']),
-            callbacks = [checkpoint_callback])
-    tend = time.time()
-    training_time = tend - tstart
-    
-    # Get predictions on validation data.
-    model.load_weights(ckpt_path)
-    model_predictions = model.predict(x=[val_data['inputs'], val_data['x0']])
-    val_predictions = SimData(t=None, x=None, u=None,
-                              y=model_predictions.squeeze())
-
-    # Return the NN controller.
-    return (model, training_time, val_predictions)
+    return (cost_plant, cost_greybox, us)
 
 def main():
     """ Main function to be executed. """
@@ -65,26 +98,14 @@ def main():
                                          'tworeac_parameters_lin.pickle',
                                          type='read')
     # Create the hybrid model.
-    Np = 3
-    fnn_dims = [8, 2]
-    tworeac_model = create_tworeac_model(Np=Np, fnn_dims=fnn_dims,
-                    tworeac_parameters=tworeac_parameters['parameters'])
-    # Get the training data.
-    (train_data, trainval_data, val_data) = get_tworeac_train_val_data(Np=Np,
-                                parameters=tworeac_parameters['parameters'],
-                                data_list=tworeac_parameters['training_data'])
-    (tworeac_model, training_time, 
-          val_predictions) = train_model(tworeac_model, 
-                                         train_data, trainval_data, val_data,
-                                         'tworeac_train_lin.txt', 
-                                         'tworeac_train_lin.ckpt')
-    fnn_weights = tworeac_model.get_weights()
-    # Save the weights.
-    tworeac_training_data = dict(fnn_weights=fnn_weights,
-                                   val_predictions=val_predictions,
-                                   training_time=training_time)
+    #Np = 3
+    #fnn_dims = [8, 2]
+    (cost_plant, cost_greybox, us) = compute_cost_curves(parameters=
+                             tworeac_parameters['parameters'])
+    figures = plot_profit_curve(us=us, costs=[cost_plant, cost_greybox])
     # Save data.
-    PickleTool.save(data_object=tworeac_training_data, 
-                    filename='tworeac_train_lin.pickle')
+    with PdfPages('tworeac_ssopt_lin.pdf', 'w') as pdf_file:
+        for fig in figures:
+            pdf_file.savefig(fig)
 
 main()
