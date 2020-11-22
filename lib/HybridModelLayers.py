ed6aeb46f3f1df68ae7248a3cc7ba74937611c3b
Pratyush Kumar <pratyushkumar@ucsb.edu>
2020-11-22 10:31:30 -0800

    will make some edits to the Hybrid Layers.

--- lib/HybridModelLayers.py
+++ lib/HybridModelLayers.py
@@ -5,8 +5,8 @@ using neural networks.
 Pratyush Kumar, pratyushkumar@ucsb.edu
 """
 import sys
+import numpy as np
 import tensorflow as tf
-tf.keras.backend.set_floatx('float64')
 
 class TwoReacAddNNCell(tf.keras.layers.AbstractRNNCell):
     """
@@ -140,78 +140,78 @@ class BlackBoxCell(tf.keras.layers.AbstractRNNCell):
         # Return output and states at the next time-step.
         return (y, xGplus)
 
-class TwoReacResidualCell(tf.keras.layers.AbstractRNNCell):
-    """
-    RNN Cell
-    xG^+  = f_g(xG, u), y = xG + f_N(y_{k+1-N_p:k-1}, u_{k+1-N_p:k-1})
-    """
-    def __init__(self, Np, fnn_layers, tworeac_parameters, **kwargs):
-        super(TwoReacResidualCell, self).__init__(**kwargs)
-        self.Np = Np
-        self.fnn_layers = fnn_layers
-        self.tworeac_parameters = tworeac_parameters
-        (self.Ng, self.Nu) = (tworeac_parameters['Ng'],
-                              tworeac_parameters['Nu'])
-
-    @property
-    def state_size(self):
-        return self.Ng
-    
-    @property
-    def output_size(self):
-        return self.Ng        
-
-    def _fg(self, x, u):
-        """ Function to compute the 
-            derivative (RHS of the ODE)
-            for the two reaction model. """
-
-        # Extract the parameters.
-        k1 = self.tworeac_parameters['k1']
-        tau = self.tworeac_parameters['ps'].squeeze()
-        
-        # Get the state and control.
-        (Ca, Cb) = (x[..., 0:1], x[..., 1:2])
-        Ca0 = u[..., 0:1]
-        
-        # Write the ODEs.
-        dCabydt = (Ca0 - Ca)/tau - k1*Ca
-        dCbbydt = k1*Ca - Cb/tau
-
-        # Return the derivative.
-        return tf.concat([dCabydt, dCbbydt], axis=-1)
-
-    def _fnn(self, ypseq, upseq):
-        """ Compute the output of the feedforward network. """
-        fnn_output = tf.concat((ypseq, upseq), axis=-1)
-        for layer in self.fnn_layers:
-            fnn_output = layer(fnn_output)
-        return fnn_output
-
-    def call(self, inputs, states):
-        """ Call function of the hybrid RNN cell.
-            Dimension of xG: (None, Ng)
-            Dimension of input: (None, Nu+(Np-1)*(Ng+Nu))
-        """
-        # Extract variables.
-        [xG] = states
-        [u, ypseq, upseq] = tf.split(inputs, 
-                            [self.Nu, self.Np*self.Ng, self.Np*self.Nu], 
-                            axis=-1)
-        Delta = self.tworeac_parameters['sample_time']
+#class TwoReacResidualCell(tf.keras.layers.AbstractRNNCell):
+#    """
+#    RNN Cell
+#    xG^+  = f_g(xG, u), y = xG + f_N(y_{k+1-N_p:k-1}, u_{k+1-N_p:k-1})
+#    """
+#    def __init__(self, Np, fnn_layers, tworeac_parameters, **kwargs):
+#        super(TwoReacResidualCell, self).__init__(**kwargs)
+#        self.Np = Np
+#        self.fnn_layers = fnn_layers
+#        self.tworeac_parameters = tworeac_parameters
+#        (self.Ng, self.Nu) = (tworeac_parameters['Ng'],
+#                              tworeac_parameters['Nu'])
+#
+#    @property
+#    def state_size(self):
+#        return self.Ng
+#    
+#    @property
+#    def output_size(self):
+#        return self.Ng        
+#
+#    def _fg(self, x, u):
+#        """ Function to compute the 
+#            derivative (RHS of the ODE)
+#            for the two reaction model. """
+#
+#        # Extract the parameters.
+#        k1 = self.tworeac_parameters['k1']
+#        tau = self.tworeac_parameters['ps'].squeeze()
+#        
+#        # Get the state and control.
+#        (Ca, Cb) = (x[..., 0:1], x[..., 1:2])
+#        Ca0 = u[..., 0:1]
+#        
+#        # Write the ODEs.
+#        dCabydt = (Ca0 - Ca)/tau - k1*Ca
+#        dCbbydt = k1*Ca - Cb/tau
+#
+#        # Return the derivative.
+#        return tf.concat([dCabydt, dCbbydt], axis=-1)
+#
+#   def _fnn(self, ypseq, upseq):
+#        """ Compute the output of the feedforward network. """
+#        fnn_output = tf.concat((ypseq, upseq), axis=-1)
+#        for layer in self.fnn_layers:
+#            fnn_output = layer(fnn_output)
+#        return fnn_output
+#
+#    def call(self, inputs, states):
+#        """ Call function of the hybrid RNN cell.
+#            Dimension of xG: (None, Ng)
+#            Dimension of input: (None, Nu+(Np-1)*(Ng+Nu))
+#        """
+#        # Extract variables.
+#        [xG] = states
+#        [u, ypseq, upseq] = tf.split(inputs, 
+#                            [self.Nu, self.Np*self.Ng, self.Np*self.Nu], 
+#                            axis=-1)
+#        Delta = self.tworeac_parameters['sample_time']
 
         # Write-down the RK4 step.
-        k1 = self._fg(xG, u)
-        k2 = self._fg(xG + Delta*(k1/2), u)
-        k3 = self._fg(xG + Delta*(k2/2), u)
-        k4 = self._fg(xG + Delta*k3, u)
+#        k1 = self._fg(xG, u)
+#        k2 = self._fg(xG + Delta*(k1/2), u)
+#        k3 = self._fg(xG + Delta*(k2/2), u)
+#        k4 = self._fg(xG + Delta*k3, u)
         
         # Get the current output/state and the next time step.
-        y = xG + self._fnn(ypseq, upseq)
-        xGplus = xG + (Delta/6)*(k1 + 2*k2 + 2*k3 + k4)
+#        y = xG + self._fnn(ypseq, upseq)
+#        xGplus = xG + (Delta/6)*(k1 + 2*k2 + 2*k3 + k4)
 
-        # Return output and states at the next time-step.
-        return (y, xGplus)
+#        # Return output and states at the next time-step.
+#        return (y, xGplus)
 
 class InterpolationLayer(tf.keras.layers.Layer):
     """
