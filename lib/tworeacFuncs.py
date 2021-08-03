0f57110f727782004ed8005a6721ad230cefc8da
Pratyush Kumar <pratyushkumar@ucsb.edu>
2021-08-03 12:28:15 -0700

    ok, went through the two reaction funcs in the lib directory.

--- lib/tworeacFuncs.py
+++ lib/tworeacFuncs.py
@@ -1,58 +1,37 @@
-""" Script to generate the necessary 
-    parameters and training data for the 
-    three reaction example.
-    Pratyush Kumar, pratyushkumar@ucsb.edu """
-
 import numpy as np
 import mpctools as mpc
 
 def plant_ode(x, u, p, parameters):
     """ Simple ODE describing a 3D system. """
+
     # Extract the parameters.
     k1 = parameters['k1']
     k2 = parameters['k2']
     k3 = parameters['k3']
-    
+    V = parameters['V']
+
     # Extract the plant states into meaningful names.
     Ca, Cb, Cc = x[0], x[1], x[2] 
     Caf = u[0]
-    tau = p[0]
+    F = p[0]
 
     # Write the ODEs.
-    dCabydt = (Caf-Ca)/tau - k1*Ca
-    dCbbydt = k1*Ca - 3*k2*(Cb**3) + 3*k3*Cc - Cb/tau
-    dCcbydt = k2*(Cb**3) - k3*Cc - Cc/tau
+    dCabydt = F*(Caf - Ca)/V - k1*Ca
+    dCbbydt = k1*Ca - 3*k2*(Cb**3) + 3*k3*Cc - F*Cb/V
+    dCcbydt = k2*(Cb**3) - k3*Cc - F*Cc/V
 
-    # Return the derivative.
+    # Return.
     return mpc.vcat([dCabydt, dCbbydt, dCcbydt])
 
-# def greybox_ode(x, u, p, parameters):
-#     """ Simple ODE describing the grey-box plant. """
-
-#     # Extract the parameters.
-#     k1 = parameters['k1']
-
-#     # Extract the plant states into meaningful names.
-#     (Ca, Cb) = x[0:2]
-#     Caf = u[0]
-#     tau = p[0]
-
-#     # Write the ODEs.
-#     dCabydt = (Caf-Ca)/tau - k1*Ca
-#     dCbbydt = k1*Ca - Cb/tau
-
-#     # Return the derivative.
-#     return np.array([dCabydt, dCbbydt])
-
 def get_plant_pars():
-    """ Get the parameter values for the 
-        three reaction example. """
+    """ Plant model parameters. """
     
     # Parameters.
     parameters = {}
     parameters['k1'] = 6e-2 # m^3/min.
     parameters['k2'] = 4e-1 # m^3/min.
     parameters['k3'] = 8e-1 # m^3/min.
+    parameters['V'] = 10 # m^3
 
     # Store the dimensions.
     parameters['Nx'] = 3
@@ -64,9 +43,9 @@ def get_plant_pars():
     parameters['Delta'] = 1. # min.
 
     # Get the steady states.
-    parameters['xs'] = np.array([1., 0.5, 0.5]) # to be updated.
-    parameters['us'] = np.array([1.]) # Ca0s
-    parameters['ps'] = np.array([20.]) # min.
+    parameters['xs'] = np.array([1., 0.5, 0.5]) # to be rectified.
+    parameters['us'] = np.array([1.]) # mol/m^3
+    parameters['ps'] = np.array([0.5]) # m^3/min
 
     # Get the constraints. 
     ulb = np.array([0.2])
@@ -76,33 +55,31 @@ def get_plant_pars():
 
     # Measurement indices and noise.
     parameters['yindices'] = [0, 1, 2]
-    parameters['Rv'] = np.diag([1e-4, 1e-3, 1e-4])
+    parameters['Rv'] = np.diag([1e-4, 1e-4, 1e-4])
 
     # Return the parameters dict.
     return parameters
 
 def get_hyb_greybox_pars(*, plant_pars):
-    """ Get the parameter values for the 
-        three reaction example. """
+    """ GreyBox parameters for the hybrid model. """
     
     # Parameters.
     parameters = {}
-    #parameters['k1'] = 0.2 # m^3/min.
 
-    # Store the dimensions.
+    # Model dimensions.
     parameters['Nx'] = 3
     parameters['Nu'] = plant_pars['Nu']
     parameters['Ny'] = plant_pars['Ny']
     parameters['Np'] = plant_pars['Np']
 
     # Sample time.
-    parameters['Delta'] = plant_pars['Delta'] # min.
+    parameters['Delta'] = plant_pars['Delta']
 
     # Get the steady states.
     gb_indices = [0, 1, 2]
-    parameters['xs'] = plant_pars['xs'][gb_indices] # to be updated.
-    parameters['us'] = plant_pars['us'] # Cafs
-    parameters['ps'] = plant_pars['ps'] # tau (min)
+    parameters['xs'] = plant_pars['xs'][gb_indices] # to be rectified.
+    parameters['us'] = plant_pars['us']
+    parameters['ps'] = plant_pars['ps'] 
 
     # Get the constraints.
     parameters['ulb'] = plant_pars['ulb']
@@ -112,43 +89,45 @@ def get_hyb_greybox_pars(*, plant_pars):
     return parameters
 
 def cost_yup(y, u, p):
-    """ Custom stage cost for the tworeac system. """
-    # Get inputs, parameters, and states.
+    """ Economic stage cost. """
+
+    # Get inputs, cost parameters, and measurements.
     CAf = u[0:1]
-    ca, cb = p[0:2]
-    CA, CB = y[0:2]
-    # Compute and return cost.
-    return ca*CAf - cb*CB
+    costPar_A, costPar_B = p[0:2]
+    _, CB = y[0:2]
 
-def getEconDistPars(seed=2):
-    """ Get the economic and measured disturbance parameters. """
+    # Return.
+    return costPar_A*CAf - costPar_B*CB
 
-    # Set random number seed.
-    np.random.seed(seed)
+# def getEconDistPars(seed=2):
+#     """ Get the economic and measured disturbance parameters. """
 
-    # Number of simulation steps.
-    Nsim = 6*24*60 # 6 days.
+#     # Set random number seed.
+#     np.random.seed(seed)
 
-    # Economic cost parameters.
-    NParChange = 4*60
+#     # Number of simulation steps.
+#     Nsim = 6*24*60 # 6 days.
 
-    # Gather two sets of parameters.
-    # In the more constrained region.
-    plb = np.array([100., 200.])
-    pub = np.array([100., 400.])
-    econPars1 = (pub - plb)*np.random.rand(Nsim//NParChange//2, 2) + plb
-    plb = np.array([100., 100.])
-    pub = np.array([100., 600.])
-    econPars2 = (pub - plb)*np.random.rand(Nsim//NParChange//2, 2) + plb
-    econPars = np.concatenate((econPars1, econPars2), axis=0)
-    np.random.shuffle(econPars)
+#     # Economic cost parameters.
+#     NParChange = 4*60
 
-    # Now repeat.
-    econPars = np.repeat(econPars, NParChange, axis=0)
+#     # Gather two sets of parameters.
+#     # In the more constrained region.
+#     plb = np.array([100., 200.])
+#     pub = np.array([100., 400.])
+#     econPars1 = (pub - plb)*np.random.rand(Nsim//NParChange//2, 2) + plb
+#     plb = np.array([100., 100.])
+#     pub = np.array([100., 600.])
+#     econPars2 = (pub - plb)*np.random.rand(Nsim//NParChange//2, 2) + plb
+#     econPars = np.concatenate((econPars1, econPars2), axis=0)
+#     np.random.shuffle(econPars)
 
-    # Measured disturbance parameters.
-    ps = get_plant_pars()['ps'][:, np.newaxis]
-    distPars = np.tile(ps.T, (Nsim, 1))
+#     # Now repeat.
+#     econPars = np.repeat(econPars, NParChange, axis=0)
 
-    # Return. 
-    return econPars, distPars
+#     # Measured disturbance parameters.
+#     ps = get_plant_pars()['ps'][:, np.newaxis]
+#     distPars = np.tile(ps.T, (Nsim, 1))
+
+#     # Return. 
+#     return econPars, distPars
