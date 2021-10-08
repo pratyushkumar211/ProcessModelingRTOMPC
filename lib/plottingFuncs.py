edd4d8b6339d1ccaa0d0be028a62e60cb63606ad
Pratyush Kumar <pratyushkumar@ucsb.edu>
2021-10-08 15:11:17 -0700

    rename a file.

---
+++ lib/plottingFuncs.py
@@ -0,0 +1,242 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib
+import itertools
+
+PRESENTATION_FIGSIZE = (6, 6)
+PAPER_FIGSIZE = (5, 5)
+
+def get_plotting_arrays(simdata, plot_range):
+    """ Get data and return for plotting. """
+    start, end = plot_range
+    u = simdata.u[start:end, :]
+    x = simdata.x[start:end, :]
+    y = simdata.y[start:end, :]
+    p = None#simdata.p[start:end, :]
+    t = simdata.t[start:end]
+    # Return u, x, y, p, t.
+    return (u, x, y, p, t)
+
+def get_plotting_array_list(*, simdata_list, plot_range):
+    """ Get all data as lists. """
+    ulist, xlist, ylist, plist = [], [], [], []
+    for simdata in simdata_list:
+        u, x, y, p, t = get_plotting_arrays(simdata, plot_range)
+        ulist += [u]
+        xlist += [x]
+        ylist += [y]
+        plist += [p]
+    # Return lists.
+    return (t, ulist, xlist, ylist, plist)
+
+#def plot_sub_gaps(*, num_samples, sub_gaps, colors, legends, 
+#                  figure_size=PAPER_FIGSIZE,
+#                  ylabel_xcoordinate=-0.11, 
+#                  left_label_frac=0.15):
+#    """ Plot the suboptimality gaps. """
+#    (figure, axes) = plt.subplots(nrows=1, ncols=1, 
+#                                  sharex=True, 
+#                                  figsize=figure_size, 
+#                                  gridspec_kw=dict(left=left_label_frac))
+#    ylabel = r'$\% \ $ Suboptimality Gap'
+#    xlabel = 'Hours of training samples'
+#    num_samples = num_samples/60
+#    for (sub_gap, color) in zip(sub_gaps, colors):
+#        # Plot the corresponding data.
+#        axes.plot(num_samples, sub_gap, color)
+#    axes.legend(legends)
+#    axes.set_xlabel(xlabel)
+#    axes.set_ylabel(ylabel)
+#    axes.get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5) 
+#    axes.set_xlim([np.min(num_samples), np.max(num_samples)])
+#    # Return the figure object.
+#    return [figure]
+
+# class CstrFlashPlots:
+
+#     ylabels = [r'$H_r \ (\textnormal{m})$',
+#                r'$C_{Br} \ (\textnormal{mol/m}^3)$', 
+#                r'$T_r \ (K)$',
+#                r'$H_b \ (\textnormal{m})$',
+#                r'$C_{Bb} \ (\textnormal{mol/m}^3)$',
+#                r'$T_b \ (K)$']
+
+#     xlabels = [r'$H_r \ (\textnormal{m})$',
+#                r'$C_{Ar} \ (\textnormal{mol/m}^3)$', 
+#                r'$C_{Br} \ (\textnormal{mol/m}^3)$',
+#                r'$C_{Cr} \ (\textnormal{mol/m}^3)$',
+#                r'$T_r \ (K)$',
+#                r'$H_b \ (\textnormal{m})$',
+#                r'$C_{Ab} \ (\textnormal{mol/m}^3)$',
+#                r'$C_{Bb} \ (\textnormal{mol/m}^3)$',
+#                r'$C_{Cb} \ (\textnormal{mol/m}^3)$',
+#                r'$T_b \ (K)$']
+
+#     ulabels = [r'$F \ (\textnormal{m}^3/\textnormal{min})$',
+#                r'$D \ (\textnormal{m}^3/\textnormal{min})$']
+
+#     def plot_inputs(t, ulist, figure_size, ylabel_xcoordinate, 
+#                     plot_ulabel, legend_names, legend_colors, 
+#                     title_loc):
+#         """ Plot the training input data. """
+#         nrow = len(CstrFlashPlots.ulabels)
+#         (figure, axes) = plt.subplots(nrows=nrow, ncols=1,
+#                                       sharex=True, figsize=figure_size,
+#                                       gridspec_kw=dict(wspace=0.5))
+#         legend_handles = []
+#         for (u, color) in zip(ulist, legend_colors):
+#             for row in range(nrow):
+#                 handle = axes[row].step(t, u[:, row], color, where='post')
+#                 axes[row].set_ylabel(CstrFlashPlots.ulabels[row])
+#                 axes[row].get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5)
+#             axes[row].set_xlabel('Time (hr)')
+#             axes[row].set_xlim([np.min(t), np.max(t)])
+#             legend_handles += handle
+#         if plot_ulabel:
+#             figure.legend(handles = legend_handles,
+#                         labels = legend_names,
+#                         loc = title_loc, ncol=len(legend_names))
+#         return [figure]
+
+#     def plot_outputs(t, ylist, figure_size, ylabel_xcoordinate,
+#                      legend_names, legend_colors, title_loc):
+#         """ Plot the training input data."""
+#         nrow = len(CstrFlashPlots.ylabels)
+#         (figure, axes) = plt.subplots(nrows=nrow, ncols=1,
+#                                     sharex=True, figsize=figure_size,
+#                                     gridspec_kw=dict(wspace=0.5))
+#         legend_handles = []
+#         for (y, color) in zip(ylist, legend_colors):
+#             for row in range(nrow):
+#                 handle = axes[row].plot(t, y[:, row], color)
+#                 axes[row].set_ylabel(CstrFlashPlots.ylabels[row])
+#                 axes[row].get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5)
+#             axes[row].set_xlabel('Time (hr)')
+#             axes[row].set_xlim([np.min(t), np.max(t)])
+#             legend_handles += handle
+        
+#         if legend_names is not None:
+#             figure.legend(handles = legend_handles,
+#                         labels = legend_names,
+#                         loc = title_loc, ncol=len(legend_names))
+#         return [figure]
+
+#     def plot_states(t, xlist, figure_size, ylabel_xcoordinate,
+#                     legend_names, legend_colors, title_loc):
+#         """ Plot the training outputs. """
+#         nrow, ncol = 5, 2
+#         (figure, axes) = plt.subplots(nrows=nrow, ncols=ncol,
+#                                       sharex=True, figsize=figure_size,
+#                                       gridspec_kw=dict(wspace=0.5))
+#         legend_handles = []
+#         for (x, color) in zip(xlist, legend_colors):
+#             state_index = 0
+#             for row, col in itertools.product(range(nrow), range(ncol)):
+#                 handle = axes[row, col].plot(t, x[:, state_index], color)
+#                 axes[row, col].set_ylabel(CstrFlashPlots.xlabels[state_index])
+#                 axes[row, col].get_yaxis().set_label_coords(ylabel_xcoordinate, 
+#                                                             0.5)
+#                 if row == nrow - 1:
+#                     axes[row, col].set_xlabel('Time (hr)')
+#                     axes[row, col].set_xlim([np.min(t), np.max(t)])
+#                 state_index += 1
+#             legend_handles += handle
+#         if legend_names is not None:
+#             figure.legend(handles = legend_handles,
+#                         labels = legend_names,
+#                         loc = title_loc, ncol=len(legend_names))
+#         return [figure]
+
+#     @staticmethod
+#     def plot_data(*, t, ulist, ylist, xlist,
+#                      figure_size, u_ylabel_xcoordinate, 
+#                      x_ylabel_xcoordinate, legend_names, 
+#                      legend_colors, title_loc, y_ylabel_xcoordinate=None, 
+#                      plot_y=True, plot_ulabel=True):
+#         figures = []
+#         figures += CstrFlashPlots.plot_inputs(t, ulist, figure_size, 
+#                                u_ylabel_xcoordinate,
+#                                plot_ulabel, legend_names, legend_colors, 
+#                                title_loc)
+#         if plot_y:
+#             figures += CstrFlashPlots.plot_outputs(t, ylist, figure_size, 
+#                                    y_ylabel_xcoordinate, legend_names, 
+#                                    legend_colors, title_loc)
+#         figures += CstrFlashPlots.plot_states(t, xlist, figure_size, 
+#                                x_ylabel_xcoordinate, legend_names, 
+#                                legend_colors, title_loc)
+
+#         # Return the figure object.
+#         return figures
+
+#     @staticmethod
+#     def plot_sscosts(*, us1, us2, sscost, figure_size):
+#         """ Plot the profit curves. """
+
+#         figure, axes = plt.subplots(nrows=1, ncols=1, 
+#                                 subplot_kw=dict(projection="3d"))
+
+#         xlabel = r'$F \ (\textnormal{m}^3/\textnormal{min})$'
+#         ylabel = r'$D \ (\textnormal{m}^3/\textnormal{min})$'
+#         zlabel = r'Cost ($\$ $)'
+
+#         # Make the surface plot.
+#         surf = axes.plot_surface(us1, us2, sscost, 
+#                                     cmap=cm.viridis, 
+#                                     shade=True)
+#         #axes.view_init(10, -50)
+        
+#         figure.colorbar(surf)
+#         figure.tight_layout()
+#         axes.set_xlabel(xlabel)
+#         axes.set_ylabel(ylabel)
+#         axes.set_zlabel(zlabel, rotation=False)
+#         return [figure]
+
+def plotAvgCosts(*, t, stageCostList,
+                      legend_colors, legend_names, 
+                      figure_size=PAPER_FIGSIZE, 
+                      ylabel_xcoordinate=-0.15):
+    """ Plot the profit. """
+    (figure, axes) = plt.subplots(nrows=1, ncols=1,
+                                  sharex=True,
+                                  figsize=figure_size,
+                                  gridspec_kw=dict(left=0.15))
+    xlabel = 'Time (hr)'
+    ylabel = '$\Lambda_k$'
+    for (cost, color) in zip(stageCostList, legend_colors):
+        # Plot the corresponding data.
+        axes.plot(t, cost, color)
+    axes.legend(legend_names)
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel, rotation=True)
+    axes.get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5)
+    axes.set_xlim([np.min(t), np.max(t)])
+    # Return.
+    return [figure]
+
+# def plot_val_metrics(*, num_samples, val_metrics, colors, legends, 
+#                      figure_size=PAPER_FIGSIZE,
+#                      ylabel_xcoordinate=-0.11, 
+#                      left_label_frac=0.15):
+#     """ Plot validation metric on open loop data. """
+#     (figure, axes) = plt.subplots(nrows=1, ncols=1, 
+#                                   sharex=True, 
+#                                   figsize=figure_size, 
+#                                   gridspec_kw=dict(left=left_label_frac))
+#     xlabel = 'Hours of training samples'
+#     ylabel = 'MSE'
+#     num_samples = num_samples/60
+#     for (val_metric, color) in zip(val_metrics, colors):
+#         # Plot the corresponding data.
+#         axes.semilogy(num_samples, val_metric, color)
+#     axes.legend(legends)
+#     axes.set_xlabel(xlabel)
+#     axes.set_ylabel(ylabel)
+#     axes.get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5) 
+#     axes.set_xlim([np.min(num_samples), np.max(num_samples)])
+#     figure.suptitle('Mean squared error (MSE) - Validation data', 
+#                     x=0.52, y=0.92)
+#    # Return the figure object.
+#     return [figure]
--- reac_plots.py
+++ reac_plots.py
@@ -13,31 +13,215 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from hybridId import PickleTool
-from plottingFuncs import PAPER_FIGSIZE, ReacPlots, plotAvgCosts
-from plottingFuncs import get_plotting_array_list
+from commonPlottingFuncs import PAPER_FIGSIZE, get_plotting_array_list
 
-#def plot_sub_gaps(*, num_samples, sub_gaps, colors, legends, 
-#                  figure_size=PAPER_FIGSIZE,
-#                  ylabel_xcoordinate=-0.11, 
-#                  left_label_frac=0.15):
-#    """ Plot the suboptimality gaps. """
-#    (figure, axes) = plt.subplots(nrows=1, ncols=1, 
-#                                  sharex=True, 
-#                                  figsize=figure_size, 
-#                                  gridspec_kw=dict(left=left_label_frac))
-#    ylabel = r'$\% \ $ Suboptimality Gap'
-#    xlabel = 'Hours of training samples'
-#    num_samples = num_samples/60
-#    for (sub_gap, color) in zip(sub_gaps, colors):
-#        # Plot the corresponding data.
-#        axes.plot(num_samples, sub_gap, color)
-#    axes.legend(legends)
-#    axes.set_xlabel(xlabel)
-#    axes.set_ylabel(ylabel)
-#    axes.get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5) 
-#    axes.set_xlim([np.min(num_samples), np.max(num_samples)])
-#    # Return the figure object.
-#    return [figure]
+
+def plot_xudata(*, t, ylist, xlist, ulist, legend_names,
+                    legend_colors, figure_size,
+                    ylabel_xcoordinate, title_loc):
+    """ Plot x and u data. 
+        The states that are measured are plotted with measurement noise.
+    """
+
+    # Labels for the y axis.
+    ylabels = [r'$C_A \ (\textnormal{mol/m}^3)$',
+               r'$C_B \ (\textnormal{mol/m}^3)$',
+               r'$C_C \ (\textnormal{mol/m}^3)$',
+               r'$C_{Af} \ (\textnormal{mol/m}^3)$']
+
+    # Number of rows.
+    nrow = len(ylabels)
+
+    # Create figure/axes.
+    (figure, axes) = plt.subplots(nrows=nrow, ncols=1,
+                                  sharex=True, figsize=figure_size)
+
+    # List to store handles for title labels.
+    legend_handles = []
+
+    # Loop through all the trajectories.
+    for (y, x, u, color) in zip(ylist, xlist, ulist, legend_colors):
+        
+        # First plot the states.
+        for row in range(nrow):
+            
+            # Plot depending on the row.
+            if 0 <= row <= 1:
+                axes[row].plot(t, y[:, row], color)
+            if row == 2:
+                axes[row].plot(t, x[:, 2], color)
+            if row == 3:
+                handle = axes[row].plot(t, u[:, 0], color)
+
+            # Axes labels.
+            axes[row].set_ylabel(ylabels[row])
+            axes[row].get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5)
+        
+        # Store handles.
+        legend_handles += handle
+
+    # Overall asthetics of the x axis.
+    axes[row].set_xlabel('Time (min)')
+    axes[row].set_xlim([np.min(t), np.max(t)])
+
+    # Create a figure title with legends. 
+    if legend_names is not None:
+        figure.legend(handles = legend_handles, labels = legend_names,
+                      loc = title_loc,
+                      ncol=len(legend_names)//2)
+
+    # Return figure.
+    return [figure]
+
+def plot_xsvus(*, us, xs_list, legend_names, 
+                  legend_colors, figure_size,
+                  ylabel_xcoordinate, title_loc):
+    """ Plot steady state xs and us. """
+    
+    # Y-axis labels.
+    ylabels = [r'$C_{As} \ (\textnormal{mol/m}^3)$', 
+               r'$C_{Bs} \ (\textnormal{mol/m}^3)$',
+               r'$C_{Cs} \ (\textnormal{mol/m}^3)$']
+    xlabel = r'$C_{Afs} \ (\textnormal{mol/m}^3)$'
+
+    # Number of rows.
+    nrow = len(ylabels)
+
+    # Create figure/axes.
+    (figure, axes) = plt.subplots(nrows=nrow, ncols=1,
+                                  sharex=True, figsize=figure_size)
+    
+    # List to store handles.
+    legend_handles = []
+
+    # Loop through all the steady-state profiles.
+    for xs, color in zip(xs_list, legend_colors):
+        
+        # Plot the steady state.
+        for row in range(nrow):
+
+            # Plot the steady-state profiles.
+            handle = axes[row].plot(us, xs[:, row], color)
+            
+            # Y-axis label.
+            axes[row].set_ylabel(ylabels[row])
+            axes[row].get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5)
+        
+        # Store handle.
+        legend_handles += handle
+
+    # X-axis label and limits.
+    axes[row].set_xlabel(xlabel)
+    axes[row].set_xlim([np.min(us), np.max(us)])
+
+    # Name legends if provided. 
+    if legend_names is not None:
+        figure.legend(handles = legend_handles,
+                        labels = legend_names,
+                        loc = title_loc, ncol=len(legend_names)//2)
+
+    # Return.
+    return [figure]
+
+def plot_sscosts(*, us, sscosts, legend_colors, 
+                    legend_names, figure_size, 
+                    ylabel_xcoordinate, left_label_frac):
+    """ Plot the cost curves. """
+    
+    # Create figure and axes.
+    (figure, axes) = plt.subplots(nrows=1, ncols=1, 
+                                    sharex=True, 
+                                    figsize=figure_size, 
+                                    gridspec_kw=dict(left=left_label_frac))
+
+    # X and Y labels.
+    xlabel = r'$C_{Afs} \ (\textnormal{mol/m}^3)$'
+    ylabel = r'Cost ($\$ $)'
+
+    # Costs and legend colors.
+    for (cost, color) in zip(sscosts, legend_colors):
+
+        # Plot costs.
+        axes.plot(us, cost, color)
+
+    # Legends and labels.        
+    axes.legend(legend_names)
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.get_yaxis().set_label_coords(ylabel_xcoordinate, 0.5) 
+    
+    # Limits on x axis.
+    axes.set_xlim([np.min(us), np.max(us)])
+    
+    # Return figure.
+    return [figure]
+
+def plot_rPercentErrors(*, xGrids, yGrids, zvals, rErrors, 
+                            figure_size, xlabel, ylabel, rateTitle, 
+                            ylabel_xcoordinate, left_frac, 
+                            wspace, right_frac):
+    """ Make the plots. """
+
+    # Make plots.
+    figures = []
+    for (zval, xGrid, yGrid, rError) in zip(zvals, xGrids, yGrids, rErrors):
+        
+        # Create figures.
+        figure, axes = plt.subplots(nrows=1, ncols=1, 
+                                    sharex=True, figsize=figure_size,
+                                    gridspec_kw=dict(left=left_frac, 
+                                                        right=right_frac,
+                                                        wspace=wspace))
+
+        # Contour plot.
+        mesh = axes.pcolormesh(xGrid, yGrid, rError, cmap='viridis')
+        figure.colorbar(mesh, ax=axes)
+
+        # X and Y labels.
+        axes.set_ylabel(ylabel)
+        axes.set_xlabel(xlabel)
+
+        # Limits.
+        axes.set_xlim([np.min(xGrid), np.max(xGrid)])
+        axes.set_ylim([np.min(yGrid), np.max(yGrid)])
+
+        # Title.
+        title = rateTitle + str(zval) + ' (mol/m$^3$)' 
+        axes.set_title(title)
+
+        # Add into the figures list.
+        figures += [figure]
+
+    # Return the figure.
+    return figures
+
+def plot_ErrorHistogram(*, rErrors, figure_size, xlabel, ylabel, 
+                            left_frac, nBins, legend_names,
+                            xlims):
+    """ Make the plots. """
+    
+    # Create figures.
+    figure, axes = plt.subplots(nrows=1, ncols=1, 
+                                figsize=figure_size,
+                                gridspec_kw=dict(left=left_frac))
+
+    # Contour plot.
+    for rError in rErrors:
+        axes.hist(rError, bins=nBins)
+
+    # Legend. 
+    if legend_names is not None:
+        axes.legend(legend_names)
+
+    # X and Y labels.
+    axes.set_ylabel(ylabel)
+    axes.set_xlabel(xlabel)
+
+    # X and Y limits.
+    axes.set_xlim(xlims)
+
+    # Return the figure.
+    return [figure]
 
 def plot_cost_pars(t, cost_pars,
                    figure_size=PAPER_FIGSIZE, 
@@ -111,25 +295,17 @@ def main():
                                                  plot_range=(2, 6*60+10))
 
         # xu data.
-        figures += ReacPlots.plot_yxudata(t=t, ylist=ylist, 
-                                            xlist=xlist, ulist=ulist,
-                                            legend_names=None,
-                                            legend_colors=['b'], 
-                                            figure_size=PAPER_FIGSIZE, 
-                                            ylabel_xcoordinate=-0.1, 
-                                            title_loc=None)
-
-        # yup data.
-        # figures += ReacPlots.plot_yupdata(t=t, ylist=ylist, ulist=ulist,
-        #                                      plist=plist,
-        #                                     legend_names=None,
-        #                                     legend_colors=['b'], 
-        #                                     figure_size=PAPER_FIGSIZE, 
-        #                                     ylabel_xcoordinate=-0.1, 
-        #                                     title_loc=None)
+        figures += plot_xudata(t=t, ylist=ylist, 
+                                xlist=xlist, ulist=ulist,
+                                legend_names=None,
+                                legend_colors=['b'], 
+                                figure_size=PAPER_FIGSIZE, 
+                                ylabel_xcoordinate=-0.1, 
+                                title_loc=None)
 
     # Plot validation data.
-    legend_names = ['Plant', 'Black-Box-NN', 'Hybrid-1', 'Hybrid - 2']
+    legend_names = ['Plant', 'Black-Box-NN', 
+                    'Hybrid - FullGb', 'Hybrid - PartialGb']
     legend_colors = ['b', 'dimgrey', 'm', 'g']
     valdata_plant = reac_parameters['training_data'][-1]
     valdata_list = [valdata_plant]
@@ -146,50 +322,42 @@ def main():
     ulist += ulist_val
     ylist += ylist_val
     xlist += xlist_val
-    figures += ReacPlots.plot_yxudata(t=t, ylist=ylist, 
-                                      xlist=xlist, ulist=ulist,
-                                        legend_names=legend_names,
-                                        legend_colors=legend_colors, 
-                                        figure_size=PAPER_FIGSIZE, 
-                                        ylabel_xcoordinate=-0.1, 
-                                        title_loc=(0.12, 0.9))
-
-    # Plot validation metrics to show data requirements.
-    #num_samples = reac_train['num_samples']
-    #val_metrics = reac_train['val_metrics']
-    #figures += plot_val_metrics(num_samples=num_samples,
-    #                            val_metrics=val_metrics, 
-    #                            colors=['dimgray', 'm'], 
-    #                            legends=['Black-box', 'Hybrid'])
+    figures += plot_xudata(t=t, ylist=ylist, 
+                            xlist=xlist, ulist=ulist,
+                            legend_names=legend_names,
+                            legend_colors=legend_colors, 
+                            figure_size=PAPER_FIGSIZE, 
+                            ylabel_xcoordinate=-0.1, 
+                            title_loc=(0.25, 0.9))
 
     # Steady state Concentrations.
     us = reac_ssopt['us']
     Ny = reac_parameters['plant_pars']['Ny']
     xs_list = reac_ssopt['xs']
     Nss_data = xs_list[0].shape[0]
-    #xs_list[1] = np.concatenate((xs_list[1][:, :Ny], 
-    #                             np.tile(np.nan, (Nss_data, 1))), axis=-1)
-    xs_list.pop(1)
-    xs_list[2] = np.concatenate((xs_list[2][:, :Ny], 
+    xs_list[1] = np.concatenate((xs_list[1][:, :Ny], 
                                  np.tile(np.nan, (Nss_data, 1))), axis=-1)
-    legend_names = ['Plant', 'Hybrid-1', 'Hybrid-2']
-    legend_colors = ['b', 'm', 'g']
-    figures += ReacPlots.plot_xsvus(us=us, xs_list=xs_list, 
-                                        legend_colors=legend_colors, 
-                                        legend_names=legend_names, 
-                                        figure_size=PAPER_FIGSIZE, 
-                                        ylabel_xcoordinate=-0.12, 
-                                        title_loc=(0.25, 0.9))
+    xs_list[3] = np.concatenate((xs_list[3][:, :Ny], 
+                                 np.tile(np.nan, (Nss_data, 1))), axis=-1)
+    legend_names = ['Plant', 'Black-Box-NN', 
+                    'Hybrid - FullGb', 'Hybrid - PartialGb']
+    legend_colors = ['b', 'dimgrey', 'm', 'g']
+    figures += plot_xsvus(us=us, xs_list=xs_list, 
+                          legend_colors=legend_colors, 
+                          legend_names=legend_names, 
+                          figure_size=PAPER_FIGSIZE, 
+                          ylabel_xcoordinate=-0.12, 
+                          title_loc=(0.25, 0.9))
 
-    # Steady state cost curves.
-    sscosts = reac_ssopt['sscosts']
-    sscosts.pop(1)
-    figures += ReacPlots.plot_sscosts(us=us, sscosts=sscosts, 
-                                        legend_colors=legend_colors, 
-                                        legend_names=legend_names, 
-                                        figure_size=PAPER_FIGSIZE, 
-                                        ylabel_xcoordinate=-0.12, 
-                                        left_label_frac=0.15)
+    # # Steady state cost curves.
+    # sscosts = reac_ssopt['sscosts']
+    # sscosts.pop(1)
+    # figures += ReacPlots.plot_sscosts(us=us, sscosts=sscosts, 
+    #                                     legend_colors=legend_colors, 
+    #                                     legend_names=legend_names, 
+    #                                     figure_size=PAPER_FIGSIZE, 
+    #                                     ylabel_xcoordinate=-0.12, 
+    #                                     left_label_frac=0.15)
 
     # Make the histograms.
     # fullgbErrors = reac_fullgbRateAnalysis[0]
