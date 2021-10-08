de01463fa64d8289b866d7866c67f728510ed4f4
Pratyush Kumar <pratyushkumar@ucsb.edu>
2021-10-08 11:06:37 -0700

    working on the rate analysis script.

--- reac_rateanalysis.py
+++ reac_rateanalysis.py
@@ -1,6 +1,5 @@
 # [depends] tworeac_parameters.pickle
 # [depends] tworeac_hybtrain.pickle
-
 import sys
 sys.path.append('lib/')
 import itertools
@@ -8,52 +7,87 @@ import numpy as np
 from hybridId import PickleTool
 from BlackBoxFuncs import fnn
 
-def doRateAnalysis(*, xrange, yrange, zval, 
-                      fNWeights, xuyscales, k, reaction):
-    """ Function to do rate analysis. 
-        x: Ca, y:Cb, z:Cc
+def getTrueR1R2(*, Ca, Cb, Cc, k1, k2f, k2b):
+    """ Function to get the true reaction rates. """
+
+    # Get r1.
+    r1 = k1*Ca
+
+    # Get r2. 
+    r2 = k2f*(Cb**3) - k2b*Cc
+
+    # Return.
+    return r1, r2
+
+def getFullGbR1R2(*, Ca, Cb, Cc, r1Weights, r2Weights, xuyscales):
+    """ Function to get the rate laws for the full GB hybrid model. 
+        Rates for this hybrid model are parametrized such that
+        r1 = NN_r1(Ca), r2 = NN_r2(Cb, Cc)
     """
 
-    # Get a concatenated set of state values.
-    Nx = xrange.shape[0]
-    Ny = yrange.shape[0]
+    # Get scaling.
+    ymean, ystd = xuyscales['yscale']
+    Camean, Cbmean = ystd[0:1], ystd[1:2]
+    Castd, Cbstd = ystd[0:1], ystd[1:2]
 
-    # Get a meshgrid of X and Y values.
-    xGrid, yGrid = np.meshgrid(xrange, yrange)
+    # Get r1.
+    Ca = (Ca - Camean)/Castd
+    r1 = fnn(Ca, r1Weights)*Castd 
 
-    # Scale.
-    xmean, xstd = xuyscales['yscale']
-    Castd, Cbstd, Ccstd = xstd[0:1], xstd[1:2], xstd[2:3]
-    umean, ustd = xuyscales['uscale']
+    # Get r2.
+    Cb = (Cb - Cbmean)/Cbstd
+    Cc = (Cc - Cbmean)/Cbstd
+    CbCc = np.concatenate((Cb, Cc))
+    r2 = fnn(CbCc, r2Weights)*Cbstd 
 
-    # Get the NN outputs.
-    r = np.tile(np.nan, (Ny, Nx))
-    rNN = np.tile(np.nan, (Ny, Nx))
-    rError = np.tile(np.nan, (Ny, Nx))
-    for i, j in itertools.product(range(Ny), range(Nx)):
+    # Return.
+    return r1, r2
 
-        # Compute the true reaction rate.
-        if reaction == 'First':
-            r[i, j] = k*xGrid[i, j]
-        else:
-            r[i, j] = k*(yGrid[i, j]**3)
+def getPartialGbR1R2(*, Ca, Cb, z, r1Weights, r2Weights, xuyscales):
+    """ Function to get the rate laws for the partial GB hybrid model. 
+        Rates for this hybrid model are parametrized such that
+        r1 = NN_r1(Ca), r2 = NN_r2(Cb, z)
+    """
 
-        # Get the NN outputs.
-        x = np.array([xGrid[i, j], yGrid[i, j], zval])
-        x = (x - xmean)/xstd
-        nnOutput = fnn(x, fNWeights)
+    # Get scaling.
+    ymean, ystd = xuyscales['yscale']
+    Camean, Cbmean = ystd[0:1], ystd[1:2]
+    Castd, Cbstd = ystd[0:1], ystd[1:2]
+    zmean = np.concatenate((np.tile(ymean, (Np, )), 
+                             np.tile(umean, (Np, ))))
+    zstd = np.concatenate((np.tile(ystd, (Np, )), 
+                            np.tile(ustd, (Np, ))))
 
-        # Get the reaction rates.
-        if reaction == 'First':
-            rNN[i, j] = nnOutput[0:1]*Castd
-        else:
-            rNN[i, j] = nnOutput[1:2]*Ccstd
+    # Get r1.
+    Ca = (Ca - Camean)/Castd
+    r1 = fnn(Ca, r1Weights)*Castd 
 
-        # Get the error.
-        rError[i, j] = np.abs(rNN[i, j] - r[i, j])/r[i, j]
+    # Get r2.
+    Cb = (Cb - Cbmean)/Cbstd
+    z = (z - zmean)/zstd
+    r2Input = np.concatenate((Cb, z))
+    r2 = fnn(r2Input, r2Weights)*Cbstd
 
-    # Return the data.
-    return xGrid, yGrid, r, rNN, rError
+    # Return.
+    return r1, r2
+
+def getFullGbRateErrorsInStateSpace(*, CaRange, CbRange, CcRange, 
+                                     r1Weights, r2Weights, xuyscales, 
+                                     k1, k2f, k2b):
+    """ Get errors in the reaction rate laws of the 
+        Full Grey-Box model in a chosen region of state-space. """
+
+
+    # Return. 
+    return 
+
+def getFullGbRateErrorsOnGeneratedData(*, training_data, r1Weights,
+                                          r2Weights, xuyscales, k1, k2f, k2b):
+    """ Get errors in the reaction rate laws of the 
+        full Grey-Box model on entire generated data. """ 
+
+    # Return. 
+    return 
 
 def getRateErrorsOnTrainingData(*, training_data_dyn, 
                                    fNWeights, xuyscales, 
@@ -106,12 +140,15 @@ def getRateErrorsOnTrainingData(*, training_data_dyn,
 def main():
     """ Main function to be executed. """
 
-    # Load parameters and training data.
+    # Load parameters.
     tworeac_parameters = PickleTool.load(filename=
                                          'tworeac_parameters.pickle',
                                          type='read')
+    tworeac_partialgbtrain = PickleTool.load(filename=
+                                      'tworeac_partialgbtrain.pickle',
+                                      type='read')
     tworeac_hybtrain = PickleTool.load(filename=
-                                      'tworeac_hybtrain_dyndata.pickle',
+                                      'tworeac_hybtrain.pickle',
                                       type='read')
     
     # Neural network weights and scaling.
@@ -120,78 +157,10 @@ def main():
     xmean, xstd = xuyscales['yscale']
     umean, ustd = xuyscales['uscale']
 
-    # Rate constant parameters.
-    k1 = tworeac_parameters['plant_pars']['k1']
-    k2 = tworeac_parameters['plant_pars']['k2']
 
-    # Get the neural network reaction rates.
-    CcVals = [0.09, 0.4]
-    CaRanges = [np.arange(0.20, 0.70, 1e-2), np.arange(0.1, 0.80, 1e-2)]
-    CbRanges = [np.arange(0.15, 0.28, 1e-2), np.arange(0.1, 0.35, 1e-2)]
-    
-    # Loop over concentration of C values.
-    r1, r1NN, r1Errors = [], [], []
-    xGrids, yGrids = [], []
-    for CaRange, CbRange, CcVal in zip(CaRanges, CbRanges, CcVals):
-        
-        # Do analysis.
-        (xGrid, yGrid, 
-         r, rNN, rErrors) = doRateAnalysis(xrange=CaRange, yrange=CbRange, 
-                                           zval=CcVal, fNWeights=fNWeights, 
-                                           xuyscales=xuyscales, k=k1, 
-                                           reaction='First')
-
-        # Store data in lists.
-        r1 += [r]
-        r1NN += [rNN]
-        r1Errors += [rErrors]       
-        xGrids += [xGrid]
-        yGrids += [yGrid]
-
-    # r1 Analysis data.
-    r1Data = dict(r=r1, rNN=r1NN, rErrors=r1Errors, 
-                  xGrids=xGrids, yGrids=yGrids, CcVals=CcVals)
-
-    # Ranges of Ca and Cb.
-    CaRanges = [np.arange(0.20, 0.70, 1e-2), np.arange(0.20, 0.70, 1e-2)]
-    CbRanges = [np.arange(0.15, 0.28, 1e-2), np.arange(0.15, 0.35, 1e-2)]
-
-    # Loop over concentration of C values.
-    r2, r2NN, r2Errors = [], [], []
-    xGrids, yGrids = [], []
-    for CaRange, CbRange, CcVal in zip(CaRanges, CbRanges, CcVals):
-        
-        # Do analysis.
-        (xGrid, yGrid, 
-         r, rNN, rErrors) = doRateAnalysis(xrange=CaRange, yrange=CbRange, 
-                                           zval=CcVal, fNWeights=fNWeights, 
-                                           xuyscales=xuyscales, k=k2, 
-                                           reaction='Second')
-
-        # Store data in lists.
-        r2 += [r]
-        r2NN += [rNN]
-        r2Errors += [rErrors]
-        xGrids += [xGrid]
-        yGrids += [yGrid]
-
-    # r2 Analysis data.
-    r2Data = dict(r=r2, rNN=r2NN, rErrors=r2Errors, 
-                  xGrids=xGrids, yGrids=yGrids, CcVals=CcVals)
-
-    # Errors in the rates collected over training data.
-    training_data_dyn = tworeac_parameters['training_data_dyn']
-    errorsOnTrain = getRateErrorsOnTrainingData(training_data_dyn=
-                                                training_data_dyn, 
-                                                fNWeights=fNWeights, 
-                                                xuyscales=xuyscales, 
-                                                k1=k1, k2=k2)
-
-    # Create a dictionary to save.
-    rateAnalysisData = [r1Data, r2Data, errorsOnTrain]
 
     # Make the plot.
     PickleTool.save(data_object=rateAnalysisData,
-                    filename='tworeac_rateAnalysis.pickle')
+                    filename='reac_rateanalysis.pickle')
 
 main()
