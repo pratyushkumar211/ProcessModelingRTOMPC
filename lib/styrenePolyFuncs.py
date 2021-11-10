c14dc1c7c6a7b639b7e24514d86984b5a8235b0d
Pratyush Kumar <pratyushkumar@ucsb.edu>
2021-11-09 20:38:58 -0800

    working on the polymerization example now.

--- lib/styrenePolyFuncs.py
+++ lib/styrenePolyFuncs.py
@@ -10,19 +10,24 @@ def plant_ode(x, u, p, parameters):
     k2b = parameters['k2b']
     V = parameters['V']
 
-    # Extract the plant states into meaningful names.
-    Ca, Cb, Cc = x[0], x[1], x[2]
-    Caf = u[0]
-    F = p[0]
+    # Extract states.
+    cI, cM, cS, T = x[0], x[1], x[2], x[3]
+    Tc, lam0, lam1, lam2 = x[4], x[5], x[6], x[7]
 
-    # Rate laws.
-    r1 = k1*Ca
-    r2 = k2f*(Cb**3) - k2b*Cc
+    # Extract controls.
+    QfI, QfM, QfS, Qc = u[0], u[1], u[2], u[3]
+
+    # Extract disturbances.
+    cIf, cMf, cSf, Tf = p[0], p[1], p[2], p[3]
     
-    # Write the ODEs.
-    dCabydt = F*(Caf - Ca)/V - r1
-    dCbbydt = -F*Cb/V + r1 - 3*r2
-    dCcbydt = -F*Cc/V + r2
+    # Balances for concentrations.
+    dcIbydt = (QfI*cIf - Qo*cI)/V - rI
+    dcMbydt = (QfM*cMf - Qo*cM)/V - rM
+    dcSbydt = (QfS*cSf - Qo*cS)/V - rS
+
+    # Temperature balances.
+    dTbydt = Qo*(Tf - T)/V + delHr*kp*Cm*Cp/(pho*Cp) - UA*(T - Tc)/(pho*Cp*V)
+    dTcbydt = Qo
 
     # Return.
     return mpc.vcat([dCabydt, dCbbydt, dCcbydt])
@@ -63,68 +68,3 @@ def get_plant_pars():
 
     # Return the parameters dict.
     return parameters
-
-def cost_lxup_noCc(x, u, p):
-    """ Economic stage cost without a contribution
-        of the unmeasured species C concentration. """
-
-    # Inputs.
-    CAf = u[0:1]
-
-    # Cost parameters. 
-    pA, pB = p[0:2]
-
-    # States.
-    _, CB = x[0:2]
-
-    # Return.
-    return pA*CAf - pB*CB
-
-def cost_lxup_withCc(x, u, p):
-    """ Economic stage cost with a contribution 
-        of the unmeasured species C concentration. """
-
-    # Inputs.
-    CAf = u[0:1]
-
-    # Cost parameters. 
-    pA, pB, pC = p[0:3]
-
-    # States.
-    _, CB, CC = x[0:3]
-
-    # Return.
-    return pA*CAf - pB*CB + pC*CC
-
-# def getEconDistPars(seed=2):
-#     """ Get the economic and measured disturbance parameters. """
-
-#     # Set random number seed.
-#     np.random.seed(seed)
-
-#     # Number of simulation steps.
-#     Nsim = 6*24*60 # 6 days.
-
-#     # Economic cost parameters.
-#     NParChange = 4*60
-
-#     # Gather two sets of parameters.
-#     # In the more constrained region.
-#     plb = np.array([100., 200.])
-#     pub = np.array([100., 400.])
-#     econPars1 = (pub - plb)*np.random.rand(Nsim//NParChange//2, 2) + plb
-#     plb = np.array([100., 100.])
-#     pub = np.array([100., 600.])
-#     econPars2 = (pub - plb)*np.random.rand(Nsim//NParChange//2, 2) + plb
-#     econPars = np.concatenate((econPars1, econPars2), axis=0)
-#     np.random.shuffle(econPars)
-
-#     # Now repeat.
-#     econPars = np.repeat(econPars, NParChange, axis=0)
-
-#     # Measured disturbance parameters.
-#     ps = get_plant_pars()['ps'][:, np.newaxis]
-#     distPars = np.tile(ps.T, (Nsim, 1))
-
-#     # Return. 
-#     return econPars, distPars
