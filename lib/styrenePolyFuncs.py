271ccd1e8c2f7d9983ce3002460755fb4d6c8238
Pratyush Kumar <pratyushkumar@ucsb.edu>
2021-11-18 15:09:49 -0800

    edits to the function.

--- lib/styrenePolyFuncs.py
+++ lib/styrenePolyFuncs.py
@@ -4,11 +4,32 @@ import mpctools as mpc
 def plant_ode(x, u, p, parameters):
     """ Simple ODE describing the styrene polymerization plant. """
 
-    # Extract the parameters.
-    k1 = parameters['k1']
-    k2f = parameters['k2f']
-    k2b = parameters['k2b']
+    # Extract model parameters.
+    # Rate constant parameters.
+    kd0 = parameters['kd0']
+    kp0 = parameters['kp0']
+    kfs0 = parameters['kfs0']
+    kf0 = parameters['kf0']
+    ktc0 = parameters['ktc0']
+    ktd0 = parameters['ktd0']
+    # Activation energies corresponding to the parameters.
+    Ed = parameters['Ed']
+    Ep = parameters['Ep']
+    Efs = parameters['Efs']
+    Ef = parameters['Ef']
+    Etc = parameters['Etc']
+    Etd = parameters['Etd']
+
+    # Density, volume, heat capacities, and heat of reaction. 
+    f = parameters['f']
+    UA = parameters['UA']
+    delHr = parameters['delHr']
+    pho = parameters['pho']
+    Cp = parameters['Cp']
     V = parameters['V']
+    phoc = parameters['phoc']
+    Cpc = parameters['Cpc']
+    Vc = parameters['Vc']
 
     # Extract states.
     cI, cM, cS, T = x[0], x[1], x[2], x[3]
@@ -16,10 +37,31 @@ def plant_ode(x, u, p, parameters):
 
     # Extract controls.
     QI, QM, QS, Qc = u[0], u[1], u[2], u[3]
-    
+
+    # Outflow volumetric flow rate. 
+    Qo = QI + QM + QS
+
     # Extract disturbances.
     cIf, cMf, cSf, Tf = p[0], p[1], p[2], p[3]
-    
+
+    # Get the rate constants using Arhenius law.
+    kd = kd0*np.exp(-Ed/(R*T))
+    kp = kp0*np.exp(-Ep/(R*T))
+    kfs = kfs0*np.exp(-Efs/(R*T))
+    kf = kf0*np.exp(-Ef/(R*T))
+    ktc = ktc0*np.exp(-Etc/(R*T))
+    ktd = ktd0*np.exp(-Etd/(R*T))
+
+    # Probability of propagation. 
+    alpha = kp*cM/(kp*cM + kfs*cS + kf*cM + (ktc + ktd)*cP)
+
+    # Mean polymer length concentration. 
+    cP = np.sqrt(2*f*kd*cI/(ktd + ktc))
+
+    # Reaction rates. 
+    rI = kd*cI
+    rM = kp*cM*cP
+
     # Balances for concentrations.
     dcIbydt = (QI*cIf - Qo*cI)/V - rI
     dcMbydt = (QM*cMf - Qo*cM)/V - rM
@@ -27,9 +69,9 @@ def plant_ode(x, u, p, parameters):
 
     # Temperature balances.
     dTbydt = Qo*(Tf - T)/V + delHr*rM/(pho*Cp) - UA*(T - Tc)/(pho*Cp*V)
-    dTcbydt = Qc*(Tcf - T)/Vc + (U*A)*(T - Tc)/(phoc*Cpc*Vc)
+    dTcbydt = Qc*(Tcf - T)/Vc + UA*(T - Tc)/(phoc*Cpc*Vc)
 
-    # Moment balances. 
+    # Moment balances.
     # Zeroth moment.
     dlam0bydt = (kfs*kf*cS*cM + ktd*cP)*alpha*cP + (ktc*(cP**2)/2) - (Qo*lam0/V)
 
@@ -38,8 +80,8 @@ def plant_ode(x, u, p, parameters):
     dlam1bydt = (cP/(1 - alpha))*dlam1bydt - (Qo*lam1/V)
 
     # Second moment.
-    dlam2bydt = (kfs*cS + kf*cM + ktd*cP)*(4*alpha - 3*alpha**2 + alpha**3) 
-    dlam2bydt += ktc*cP*(1 + alpha)
+    dlam2bydt = (kfs*cS + kf*cM + ktd*cP)*(4*alpha - 3*alpha**2 + alpha**3)
+    dlam2bydt += ktc*cP*(2 + alpha)
     dlam2bydt = (cP/(1 - alpha)**2)*dlam2bydt - (Qo*lam2/V)
 
     # Return.
@@ -49,26 +91,46 @@ def plant_ode(x, u, p, parameters):
 def get_plant_pars():
     """ Plant model parameters. """
     
-    # Parameters.
+    # Parameter dictionary.
     parameters = {}
-    parameters['k1'] = 2e-1 # m^3/min
-    parameters['k2f'] = 5e-1 # m^3/min
-    parameters['k2b'] = 1e-1 # m^3/min
-    parameters['V'] = 15 # m^3
+
+    # Rate constant parameters.
+    parameters['kd0'] = 5.95e+13 
+    parameters['kp0'] = 1.06e+7
+    parameters['kfs0'] = 91457.3
+    parameters['kf0'] = 53020.29
+    parameters['ktc0'] = 0.5*1.25e+9
+    parameters['ktd0'] = 0.5*1.25e+9
+    # Activation energies corresponding to the parameters.
+    parameters['Ed'] = 123853.658
+    parameters['Ep'] = 29572.898
+    parameters['Efs'] = 91457.3
+    parameters['Ef'] = 53020.29
+    parameters['Etc'] = 7017.27
+    parameters['Etd'] = 7017.27
+
+    # Density, volume, heat capacities, and heat of reaction. 
+    parameters['f'] = 0.6
+    parameters['UA'] = 293.076
+    parameters['delHr'] = 69919.56
+    parameters['phoCp'] = 1506
+    parameters['V'] = 3000
+    parameters['phocCpc'] = 4045
+    parameters['Vc'] = 3100
 
     # Store the dimensions.
-    parameters['Nx'] = 3
-    parameters['Nu'] = 1
-    parameters['Ny'] = 2
-    parameters['Np'] = 1
+    parameters['Nx'] = 8
+    parameters['Nu'] = 4
+    parameters['Ny'] = 6
+    parameters['Np'] = 2
 
     # Sample time.
     parameters['Delta'] = 1. # min.
 
     # Get the steady states.
-    parameters['xs'] = np.array([1., 0.5, 0.5]) # to be rectified.
-    parameters['us'] = np.array([2.0]) # mol/m^3
-    parameters['ps'] = np.array([0.8]) # m^3/min
+    parameters['xs'] = np.array([1e-2,3,3,320,305,1e-4,1,1]) # (to rectify)
+    parameters['us'] = np.array([110, 450, 380, 480]) # mol/m^3
+    parameters['ps'] = np.array([330, 295]) # m^3/min
 
     # Input constraints.
     ulb = np.array([1.0])
